8b7731fef777f1a4e3634deb6c2faf662a44ebae
Qasem Alkurdi <7ksh1970@gmail.com>
2026-04-03 10:49:18 +0300

    feat(HotelCatalog): introduce search mode and response DTO for hotel catalog queries
    Signed-off-by: Qasem Alkurdi <7ksh1970@gmail.com>

---
+++ worldcup-hotel-booking/src/main/java/com/worldcup/hotelbooking/catalog/query/hotel/dto/HotelCatalogSearchMode.java
@@ -0,0 +1,8 @@
+package com.worldcup.hotelbooking.catalog.query.hotel.dto;
+
+public enum HotelCatalogSearchMode {
+    NORMAL,
+    MATCH_DEFAULT_RADIUS,
+    STADIUM_DEFAULT_RADIUS,
+    SAME_CITY_FALLBACK
+}
